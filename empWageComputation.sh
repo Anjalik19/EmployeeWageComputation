7620653951e7f3cf23221c932eff9e7ed1b0527f
Anjalik19 <Anjali.march19@gmail.com>
2020-02-12 17:54:07 +0530

    [Anjali]Refactor. refactor employee wage computation containing attendance check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 attendanceCheck(){
-echo "Enter the value"
+echo "Enter the value to check Attendance"
 read value
 random=$(( RANDOM % 2 ))
-if(($value==1));then
+if(( $value==1 ));then
 echo "Employee is Present"
-else(($value==0))
+else(( $value==0 ))
 echo "Employee is Absent"
 fi
 }
