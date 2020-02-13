5f711c017290b76a804542e52f3ac71f8f95680f
Anjalik19 <Anjali.march19@gmail.com>
2020-02-13 17:45:15 +0530

    [Anjali]Refactor. refactor useCase6-TotalWorkingHoursOrDay

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,41 +2,42 @@
 echo "Case 1 - Daily Employee Wage"
 echo "Case 2 - Part Time Employee Wage"
 read option
-attendanceCheck(){
-echo "Press 1 to check attendance"
-read value
-echo ""
-if(( $value==1 ))
-then
-return 1
-echo "Employee is Absent"
-fi
-}
+attendanceCheck()
+    {
+	echo "Press 1 to check attendance"
+	read value
+	echo ""
+	if(( $value==1 ))
+	then
+	return 1
+	echo "Employee is Absent"
+	fi
+    }
 
 fullWage()
-{
-echo "Wage per hour is 20"
-echo "Full day hour is 8"
-return 8
-}
+    {
+	echo "Wage per hour is 20"
+	echo "Full day hour is 8"
+	return 8
+    }
 partTime()
-{
-echo "Wage per hour is 20"
-echo "Part time wage is 4"
-return 4
-}
+    {
+	echo "Wage per hour is 20"
+	echo "Part time wage is 4"
+	return 4
+    }
 case $option in
 1)
 attendanceCheck
 present=0
-for((i=1;i<=20;i++))
-do
-random=$(( RANDOM % 2 ))
-if(( $random==1 ))
-then
-present=$((present+1))
-fi
-done
+   for((i=1;i<=20;i++))
+     do
+	random=$(( RANDOM % 2 ))
+	if(( $random==1 ))
+	   then
+		present=$((present+1))
+	fi
+     done
 fullWage
 result=$?
 echo "Employee Attendance is $present"
@@ -44,24 +45,24 @@ echo ""
 workingHours=$((present * result))
 echo "Working Hours are $workingHours"
 echo ""
-if(( workingHours<=100 ))
-then
-a=20
-salary=$((workingHours * a))
-echo "Monthly wages = $salary"
-fi
+     if(( workingHours<=100 ))
+	then
+	   a=20
+	   salary=$((workingHours * a))
+	   echo "Monthly wages = $salary"
+     fi
 ;;
 2)
 attendanceCheck
 present=0
-for(( i=1;i<=20;i++ ))
-do
-random=$(( RANDOM % 2 ))
-if(( $random==1 ))
-then
-present=$((present+1))
-fi
-done
+  for(( i=1;i<=20;i++ ))
+     do
+	random=$(( RANDOM % 2 ))
+	    if(( $random==1 ))
+	       then
+	       	 present=$((present+1))
+	    fi
+     done
 partTime
 result=$?
 echo "Employee Attendance is $present"
@@ -69,11 +70,11 @@ echo ""
 workingHours=$((present * 4 ))
 echo "Working hours are $workingHours"
 echo ""
-if((workingHours<=100))
-then
-b=20
-salary=$((workingHours*b))
-echo "Monthly Wages = $salary"
-fi
+  if((workingHours<=100))
+     then
+	b=20
+	salary=$((workingHours*b))
+	echo "Monthly Wages = $salary"
+  fi
 ;;
 esac
