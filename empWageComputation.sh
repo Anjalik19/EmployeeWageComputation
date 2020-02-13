98761111f7463e1e72e40dc06517d5f91bee30fb
Anjalik19 <Anjali.march19@gmail.com>
2020-02-13 11:09:55 +0530

    [Anjali]Add. added useCase4-UsingCaseStatement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,52 @@
-echo "Welcome to Employee Wage Computation"
+#!/bin/sh
+echo "Case 1 - Check Attendance"
+echo "Case 2 - Daily Employee Wage"
+echo "Case 3 - Part Time Employee Wage"
+echo "Enter your option"
+read option
+echo ""
+case $option in
+1)
+attendanceCheck(){
+echo "Attendance Check"
+echo ""
+echo "Enter the value to check Attendance"
+read value
+random=$(( RANDOM % 2 ))
+if(( $value==1));then
+echo "Employee is Present"
+else(( $value==0))
+echo "Employee is Absent"
+fi
+}
+attendanceCheck
+;;
+2)
+fullWage()
+{
+echo "Daily Employee Wage"
+echo ""
+echo "Wage per hour is 20"
+echo "Full day hour is 8"
+hour=20
+day=8
+result=$((hour * day))
+echo "Daily Employee Wage = $result"
+}
+fullWage
+;;
+3)
+partTime()
+{
+echo "Part Time Wage"
+echo ""
+echo "Wage per hour is 20"
+echo "part time hour is 4"
+hour=20
+partTime=4
+result=$((hour * partTime))
+echo "Part time Employee wage = $result"
+}
+partTime
+;;
+esac
