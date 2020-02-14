ea3a6acd92e9282dd8266c7b693dd63e2b5914c4
Anjalik19 <Anjali.march19@gmail.com>
2020-02-14 09:32:02 +0530

    [Anjali]Refactor. refactor useCase4-UsingCaseStatement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,41 +6,41 @@ echo "Enter your option"
 read option
 echo ""
 fullWage()
-{
-echo "Wage per hour is 20"
-echo "Full day hour is 8"
-return 160
-}
+   {
+	echo "Wage per hour is 20"
+	echo "Full day hour is 8"
+	return 160
+   }
 
 partTime()
-{
-echo "Wage per hour is 20"
-echo "Part time hour is 4"
-return 80
-}
+   {
+	echo "Wage per hour is 20"
+	echo "Part time hour is 4"
+	return 80
+   }
 
 attendanceCheck()
-{
-echo "Enter the value to check attendance"
-read value
-if(( $value==1 ))
-then
-return 1
-else(( $value==0 ))
-return 0
-fi
-}
+   {
+	echo "Enter the value to check attendance"
+	read value
+		if(( $value==1 ))
+		   then
+			return 1
+			else(( $value==0 ))
+			return 0
+		fi
+   }
 
 case $option in
 1)
 attendanceCheck
 result=$?
-if(($result==1))
-then
-echo "Employee is present"
-else(($result==0))
-echo "Employee is Absent"
-fi
+	if(($result==1))
+	    then
+		echo "Employee is present"
+		else(($result==0))
+		echo "Employee is Absent"
+	fi
 ;;
 2)
 fullWage
