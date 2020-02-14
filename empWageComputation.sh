757ddd415d51c3a22c8a54e5c55ffa71663416b1
Anjalik19 <Anjali.march19@gmail.com>
2020-02-14 10:54:02 +0530

    [Anjali]Refactor. refactor complete code logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -355,15 +355,8 @@ attendanceCheck()
 			   then
 				array[$i]=160
 				present=$((present+1))
-<<<<<<< HEAD
 				   else
 					array[$i]=0
-=======
-				   echo "Day $i : Present"
-				   else
-					array[$i]=0
-				    echo "Day $i : Absent"
->>>>>>> useCase9-DayAndDailyWages
 			 fi
 		done
    for((i=0;i<$size;i++))
@@ -390,15 +383,8 @@ attendanceCheck()
 			   then
 				array[$i]=160
 				present=$((present+1))
-<<<<<<< HEAD
 				   else
 					array[$i]=0
-=======
-                                echo "Day $i : Present"
-				   else
-					array[$i]=0
-                                        echo "Day $i : Absent"
->>>>>>> useCase9-DayAndDailyWages
 			fi
 		done
     for((i=0;i<$size;i++))
@@ -413,7 +399,82 @@ echo "Employee total wage $(($present*result))"
 attendanceCheck
 ;;
 esac
-<<<<<<< HEAD
 
-=======
->>>>>>> useCase9-DayAndDailyWages
+
+======
+size=20
+declare -a array[$size]
+echo "Enter 1 for Full Wages"
+echo "Enter 2 for Half Wages"
+read option
+
+fullWage()
+  {
+    return 160
+  }
+partTime()
+  {
+    return 80
+  }
+
+case $option in
+1)
+attendanceCheck()
+    {
+	present=0
+	echo ""
+	      for((i=0;i<$size;i++))
+		 do
+		     random=$(( RANDOM % 2 ))
+			if(( $random==1 ))
+			   then
+				array[$i]=160
+				present=$((present+1))
+				   echo "Day $i : Present"
+				   else
+					array[$i]=0
+				    echo "Day $i : Absent"
+			 fi
+		done
+   for((i=0;i<$size;i++))
+      do
+	 printf ${array[i]}" "
+      done
+echo ""
+fullWage
+result=$?
+echo "Employee attandance is $present"
+echo "Employee total wage $((present*result))"
+}
+attendanceCheck
+;;
+2)
+attendanceCheck()
+    {
+	present=0
+	echo ""
+	      for((i=0;i<$size;i++))
+		 do
+		     random=$(( RANDOM % 2 ))
+			if(( $random==1 ))
+			   then
+				array[$i]=160
+				present=$((present+1))
+                                echo "Day $i : Present"
+				   else
+					array[$i]=0
+                                        echo "Day $i : Absent"
+			fi
+		done
+    for((i=0;i<$size;i++))
+      do
+	printf ${array[i]}" "
+      done
+echo ""
+partTime
+result=$?
+echo "Employee total wage $(($present*result))"
+}
+attendanceCheck
+;;
+esac
