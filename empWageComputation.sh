f183f6f8ca5ed02e148df5944276b4811c24961c
Anjalik19 <Anjali.march19@gmail.com>
2020-02-17 13:52:02 +0530

    [Anjali]Refactor. refactor useCase5_CalculatingWagesForMonth

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,7 +3,7 @@ echo "Case 1 - Daily Employee Wage"
 echo "Case 2 - Part Time Employee Wage"
 read option
 attendanceCheck()
-   {
+{
 	echo "Press 1 to check attendance"
 	read value
 	echo ""
@@ -12,25 +12,25 @@ attendanceCheck()
 		return 1
 		echo "Employee is Present"
 	fi
-   }
+}
 
 fullWage()
-   {
+{
 	echo "Wage per hour is 20"
 	echo "Full day hour is 8"
 	return 160
-   }
+}
 partTime()
-   {
+{
 	echo "Wage per hour is 20"
 	echo "Part time wage is 4"
 	return 80
-   }
+}
 case $option in
 1)
 attendanceCheck
 	present=0
-	for((i=1;i<=20;i++))
+	for((i=1;i<=30;i++))
 	    do
 		random=$(( RANDOM % 2 ))
 		    if(( $random==1 ))
@@ -47,7 +47,7 @@ fullWage
 2)
 attendanceCheck
 	present=0
-	     for(( i=1;i<=20;i++ ))
+	     for(( i=1;i<=30;i++ ))
 		do
 		   random=$(( RANDOM % 2 ))
 			if(( $random==1 ))
