3e40af49d7e231cd042781f86fecff183347e127
Anjalik19 <Anjali.march19@gmail.com>
2020-02-17 13:39:05 +0530

    [Anjali]Refactor. refactor useCase8-StoreDailyWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-size=20
+size=30
 declare -a array[$size]
 echo "Enter 1 for Full Wages"
 echo "Enter 2 for Half Wages"
 read option
 
 fullWage()
-  {
+{
     return 160
-  }
+}
 partTime()
-  {
+{
     return 80
-  }
+}
 
 case $option in
 1)
 attendanceCheck()
-    {
+{
 	present=0
 	echo ""
-	      for((i=0;i<$size;i++))
+	      for((i=1;i<$size;i++))
 		 do
 		     random=$(( RANDOM % 2 ))
 			if(( $random==1 ))
@@ -31,10 +31,10 @@ attendanceCheck()
 					array[$i]=0
 			 fi
 		done
-   for((i=0;i<$size;i++))
-      do
-	 printf ${array[i]}" "
-      done
+for((i=1;i<$size;i++))
+   do
+      printf ${array[i]}" "
+   done
 echo ""
 fullWage
 result=$?
@@ -45,24 +45,24 @@ attendanceCheck
 ;;
 2)
 attendanceCheck()
-    {
+{
 	present=0
 	echo ""
-	      for((i=0;i<$size;i++))
+	      for((i=1;i<$size;i++))
 		 do
 		     random=$(( RANDOM % 2 ))
 			if(( $random==1 ))
 			   then
-				array[$i]=160
+				array[$i]=80
 				present=$((present+1))
 				   else
 					array[$i]=0
 			fi
 		done
-    for((i=0;i<$size;i++))
-      do
-	printf ${array[i]}" "
-      done
+for((i=1;i<$size;i++))
+   do
+     printf ${array[i]}" "
+   done
 echo ""
 partTime
 result=$?
