2137ebe61691ee8060408f9870b318bd3c60ca41
Anjalik19 <Anjali.march19@gmail.com>
2020-02-17 15:34:52 +0530

    [Anjali]Refactor. refactor logic of useCase3_AddPartTimeEmployee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 partTime()
 {
-echo "Wage per Hour is 20"
-echo "Part time hour is 4"
+echo " Wage per Hour is 20 "
+echo " Part time hour is 4 "
 hour=20
 parttime=4
 result=$((hour * parttime))
